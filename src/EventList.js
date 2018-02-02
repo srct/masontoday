dab6e5fa83affd8aebead23045bfd634ea21704e
Zachary Wood <zac.wood9@gmail.com>
2018-02-01 21:37:03 -0500

    formatting PART 2

--- src/EventList.js
+++ src/EventList.js
@@ -1,40 +1,30 @@
-import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  List,
-  ListItem
-} from "react-native";
-import { fetchData } from "./data";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, FlatList, List, ListItem } from 'react-native';
+import { fetchData } from './data';
 
 class EventListItem extends Component {
-  render() {
-    return <Text style={{ fontSize: 20 }}>{this.props.location}</Text>;
-  }
+    render() {
+        return <Text style={{ fontSize: 20 }}>{this.props.location}</Text>;
+    }
 }
 
 export default class EventList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  _renderItem = ({ item }) => {
-    return <EventListItem location={item.location} />;
-  };
-
-  render() {
-    if (!this.props.data) {
-      return (
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-          <Text>Loading data...</Text>
-        </View>
-      );
+    constructor(props) {
+        super(props);
     }
-    return <FlatList data={this.props.data} renderItem={this._renderItem} />;
-  }
+
+    _renderItem = ({ item }) => {
+        return <EventListItem location={item.location} />;
+    };
+
+    render() {
+        if (!this.props.data) {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Loading data...</Text>
+                </View>
+            );
+        }
+        return <FlatList data={this.props.data} renderItem={this._renderItem} />;
+    }
 }
