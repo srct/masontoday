a4cc9ae51b130ec65989fcf15608f4bc40d08989
Zach Osman <zosman@gmu.edu>
2019-02-17 13:45:08 -0500

    More settings page work Filtering is starting to take shape

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import { AsyncStorage } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Sentry } from 'react-native-sentry';
 
 import { EventListPage, EventDetailsPage, SettingsPage } from 'masontoday/src/components';
 import { Colors } from 'masontoday/src/utils';
+
 if (!__DEV__) {
     Sentry.config('https://4f6e0cb23ab04d5a89c5d6b99b2cb6f2@sentry.io/1279094').install();
 }
+
+async function init() {
+    let value = await AsyncStorage.getItem('filter');
+    if (!value) {
+        await AsyncStorage.setItem('filter', '2');
+    }
+}
+init();
 
 const MainNavigator = createStackNavigator(
     {
--- src/components/pages/SettingsPage.js
+++ src/components/pages/SettingsPage.js
@@ -1,32 +1,72 @@
 import React from 'react';
-import { StyleSheet, View, Text, SectionList } from 'react-native';
+import { StyleSheet, View, Text, AsyncStorage, SectionList, TouchableOpacity } from 'react-native';
 
 import { Colors } from 'masontoday/src/utils';
 
 const data = [
-    { title: 'Data Options', data: ['item1', 'item2'] },
-    { title: 'About', data: ['item3', 'item4'] },
-    { title: 'Licenses', data: ['item5', 'item6'] },
+    {
+        title: 'Data Filtering',
+        data: [
+            { title: 'No Filtering', value: 0 }, //0
+            { title: 'Basic Filtering', value: 1 }, //1
+            { title: 'Medium Filtering (Recommended)', value: 2 }, //2
+            { title: 'Strong Filtering', value: 3 }, //3
+            { title: 'Only the best', value: 4 }, //4
+        ],
+    },
+    {
+        title: 'About',
+        data: [{ title: 'Medium Filtering (Recommended)' }, { title: 'Strong Filtering' }],
+    },
+    {
+        title: 'Licenses',
+        data: [{ title: 'No Filtering' }, { title: 'Medium Filtering (Recommended)' }],
+    },
 ];
 export default class SettingsPage extends React.Component {
     static navigationOptions = {
         title: 'Settings',
     };
+    state = {
+        filter: 2,
+    };
+
+    async componentDidMount() {
+        let filterOption = parseInt(await AsyncStorage.getItem('filter'));
+        this.setState({
+            filter: filterOption,
+        });
+    }
+
     render() {
         return (
             <View style={{ margin: 10, flex: 1 }}>
                 <SectionList
                     renderItem={({ item, index, section }) => (
-                        <View style={{ paddingVertical: 10, backgroundColor: '#e2e2e2' }}>
-                            <Text key={index}>{item}</Text>
-                        </View>
+                        <TouchableOpacity
+                            onPress={() => {
+                                this.setState({ filter: item.value });
+                                AsyncStorage.setItem('filter', item.value.toString());
+                            }}>
+                            <View
+                                style={{
+                                    paddingVertical: 10,
+                                    backgroundColor: item.value == this.state.filter ? '#e2e2e2' : 'white',
+                                }}>
+                                <Text style={{ paddingHorizontal: 10 }} key={index}>
+                                    {item.title}
+                                </Text>
+                            </View>
+                        </TouchableOpacity>
                     )}
                     stickySectionHeadersEnabled={false}
                     ItemSeparatorComponent={() => (
                         <View style={{ flex: 1, height: StyleSheet.hairlineWidth, backgroundColor: 'black' }} />
                     )}
                     renderSectionHeader={({ section: { title } }) => (
-                        <Text style={{ fontWeight: 'bold', color: Colors.green1, textAlign: 'center' }}>{title}</Text>
+                        <Text style={{ fontWeight: 'bold', color: Colors.green1, textAlign: 'center', padding: 10 }}>
+                            {title}
+                        </Text>
                     )}
                     sections={data}
                     keyExtractor={(item, index) => item + index}
